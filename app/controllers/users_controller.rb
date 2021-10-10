57a93f5f4a15d4d5d83808cb1851c81d375700c6
Juhong An <wednesday5028@gmail.com>
2021-10-10 12:38:37 +0900

    Add flashes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,9 +40,13 @@ class UsersController < ApplicationController
 
     def destroy
         @user.destroy
-        session[:user_id] = nil
         flash[:notice] = "Account and all associated articles successfully deleted"
-        redirect_to root_path
+        if current_user.admin?
+            redirect_to articles_path
+        else 
+            session[:user_id] = nil
+            redirect_to root_path
+        end
     end
 
     private
@@ -55,7 +59,7 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-        if current_user != @user
+        if current_user != @user and !current_user.admin?
             flash[:alert] = "You can only edit or delete your own account"
             redirect_to user_path(@user)
         end
